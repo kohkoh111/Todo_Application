6ce5874125c5bccd4ed90fe8898b203d551c5645
koki <69852106+kohkoh111@users.noreply.github.com>
2021-03-02 00:20:21 +0900

    Commited

---
+++ work/public/js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+{
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      fetch('?action=toggle', {
+        method: 'POST',
+        body: new URLSearchParams({
+          id: checkbox.dataset.id,
+          token: checkbox.dataset.token,
+        }),
+      });
+
+      checkbox.nextElementSibling.classList.toggle('done');
+    });
+  });
+
+  const deletes = document.querySelectorAll('.delete');
+  deletes.forEach(span => {
+    span.addEventListener('click', () => {
+      if (!confirm('Are you sure?')) {
+        return;
+      }
+      span.parentNode.submit();
+    });
+  });
+
+  const purge = document.querySelector('.purge');
+  purge.addEventListener('click', () => {
+    if (!confirm('Are you sure?')) {
+      return;
+    }
+    purge.parentNode.submit();
+  });
+}
